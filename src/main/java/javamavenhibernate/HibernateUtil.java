3dbee5e5221e0c6d4cd24fa9c84502ce5fa74ad2
Miguel <miguelramalho51@gmail.com>
2019-03-04 17:23:24 -0300

    conexão via hibernate ok

---
+++ src/main/java/javamavenhibernate/HibernateUtil.java
@@ -0,0 +1,35 @@
+package javamavenhibernate;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+//Estaciando a conexão com o banco
+public class HibernateUtil {
+	
+	public static EntityManagerFactory factory = null;
+	
+	static {
+		init();
+	}
+	
+	
+	private static void init() {
+		
+		try {
+			//Go to [ persistence.xml ]
+			if(factory == null) {
+				factory = Persistence.createEntityManagerFactory("java-maven-hibernate");
+			}	
+		} catch (Exception e) {
+			System.out.println(e.getMessage());
+		}
+	}
+	
+	public static EntityManager getEntityManager() {
+		
+		return factory.createEntityManager(); /*Responsavel por prover a parte de persistencia de dados  */
+	}
+	
+
+}
---
+++ src/test/java/javamavenhibernate/TesteHibernate.java
@@ -0,0 +1,12 @@
+package javamavenhibernate;
+
+import org.junit.Test;
+
+public class TesteHibernate {
+	
+	@Test
+	public void testeHibernateUtil() {
+		HibernateUtil.getEntityManager();
+	}
+
+}
