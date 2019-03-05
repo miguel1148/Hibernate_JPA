f2c84411a4bb4e427fe127a5940f12b73fff5109
Miguel <miguelramalho51@gmail.com>
2019-03-05 15:37:18 -0300

    Persistencia com Hibernate

--- src/main/java/model/UsuarioPessoa.java
+++ src/main/java/model/UsuarioPessoa.java
@@ -32,6 +32,14 @@ public class UsuarioPessoa {
 	private String senha;
 	
 	
+	private int idade;
+	public int getIdade() {
+		return idade;
+	}
+	public void setIdade(int idade) {
+		this.idade = idade;
+	}
+	
 	public Long getId() {
 		return id;
 	}
--- src/test/java/javamavenhibernate/TesteHibernate.java
+++ src/test/java/javamavenhibernate/TesteHibernate.java
@@ -2,11 +2,30 @@ package javamavenhibernate;
 
 import org.junit.Test;
 
+import dao.DaoGeneric;
+import model.UsuarioPessoa;
+
 public class TesteHibernate {
 	
 	@Test
 	public void testeHibernateUtil() {
-		HibernateUtil.getEntityManager();
+//		HibernateUtil.getEntityManager(); Criar tabela		
+//		DaoGeneric<?> dao = new DaoGeneric();
+		
+		DaoGeneric<UsuarioPessoa> dao = new DaoGeneric<UsuarioPessoa>();
+		UsuarioPessoa pessoa = new UsuarioPessoa();
+		
+		pessoa.setEmail("andreza_lima@gmail.com");
+		pessoa.setLogin("teste2");
+		pessoa.setNome("Andreza");
+		pessoa.setSobrenome("Lima");
+		pessoa.setSenha("987654321");
+		pessoa.setIdade(27);
+		
+		dao.salvar(pessoa);
+		
+
+		
 	}
 
 }
